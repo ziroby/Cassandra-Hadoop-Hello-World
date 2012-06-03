35a236c20f44015917757e34ebca6aa56b529151
Ron 'Ziroby' Romero <ziroby@gmail.com>
2012-06-02 20:16:52 -0500

    Got hadoop up and running.

---
+++ helloHadoop/src/test/java/com/ziroby/helloHadoop/AppTest.java
@@ -0,0 +1,18 @@
+package com.ziroby.helloHadoop;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@org.junit.Test
+	public void testVerify() throws Exception {
+		assertThat(1, is(1));
+	}
+}
